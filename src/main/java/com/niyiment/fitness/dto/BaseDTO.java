33edecfed5c39259b65daddc8474b1b3fc470070
Niyi Ogungbemi <niyiment@gmail.com>
2025-02-04 13:58:30 +0100

    Add workout dto

---
+++ src/main/java/com/niyiment/fitness/dto/BaseDTO.java
@@ -0,0 +1,4 @@
+package com.niyiment.fitness.dto;
+
+public class BaseDTO {
+}
---
+++ src/main/java/com/niyiment/fitness/dto/WorkoutResponseDTO.java
@@ -0,0 +1,10 @@
+package com.niyiment.fitness.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class WorkoutReponseDTO extends BaseAuditDTO {
+
+}
